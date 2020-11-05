313bd79e8a4fef94f5c229916ba156b24d690226
ZPLNco <45364761+zplnco@users.noreply.github.com>
2020-11-05 16:27:02 -0500

    Update most_cited.py
    v3

--- scholar/most_cited.py
+++ scholar/most_cited.py
@@ -1,29 +1,36 @@
 import requests as req
 from bs4 import BeautifulSoup as soup
+from scraper_api import ScraperAPIClient
 
 keyword = "psychology"
 since = 2010 # start year
-pages = 15 # pages searching through
-top_x = 5 # top __ pages you wish to see
+pages = 2 # pages searching through
+top_x = 2 # top __ pages you wish to see
 
 pages_f = [n*10 for n in range(pages)] # formatting articles by every 10
 articles = [] 
 count = 0 # counting first 10 sufficient articles
 
 for page in pages_f:
-	headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
 	url = "https://scholar.google.com/scholar?start={}&q={}&hl=en&as_sdt=0,33&as_ylo={}&as_yhi=2020".format(str(page), keyword, str(since))
 
-	response = req.get(url, headers=headers)
+	client = ScraperAPIClient('bb129ca384c265942abc9af857eb1471')
+	response = client.get(url, timeout=60) 
 	resp_parsed = soup(response.content,'lxml')
 
 	if "Our systems have detected unusual traffic from your computer network." in resp_parsed.text: # checking for IP block
 		print('! IP Blocked !')
+	elif resp_parsed.status_code == 403:
+		print('! exceeded avaliable API calls !')
+	elif resp_parsed.status_code == 429:
+		print('! exceeded concurrent-connections limit !') # slow down request rate
+	elif resp_parsed.status_code == 500:
+		print('! request failed !')
 	else:
 		pass
 
 	for article in resp_parsed.select('[data-rp]'): # initializing socket connection
-		try:
+		try: 
 			title = article.select('h3')[0].text
 			if 'BOOK' in title.split()[0] or 'CITATION' in title.split()[0]:
 				continue
@@ -36,7 +43,7 @@ for page in pages_f:
 				link = article.select('a')[0].get('href')
 				
 				if count <= top_x: # after we have our top 10, the point of reference will be the lowest # of citations in the list
-					articles.append([title, cites, link])
+					articles.append([title, cites, link, int(page/10)+1])
 					articles.sort(key=lambda e: e[1], reverse=True) # sorting by # of citations
 				else:
 					if cites > articles[-1][1]:
@@ -51,8 +58,8 @@ for page in pages_f:
 			pass
 
 if __name__ == "__main__":
-	print("\n\n Keyword: {}\t\tSince: {}\t\tArticles Analyzed: {}\n\n".format(keyword, str(since), str(pages)))
+	print("\n\n Keyword: {}\t\tSince: {}\t\tArticles Analyzed: {}\n\n".format(keyword, str(since), str(pages*10)))
 	for a in articles:
-		print("\n TITLE: {}\n\n CITES: {}\n\n URL: {}".format(a[0], a[1], a[2]), end='\n-----------------------------------------\n')
+		print("\n TITLE: {}\n\n CITES: {}\n\n URL: {}\n\n PAGE: {}".format(a[0], a[1], a[2], a[3]), end='\n-----------------------------------------\n')
 
 		
